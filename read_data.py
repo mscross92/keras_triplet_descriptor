d2b01186b47afe5d9142fd329415b8fbe6fc5904
Maria Cross <mscross92@googlemail.com>
2019-06-25 17:39:36 +0100

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -164,6 +164,7 @@ def get_hard_negs(desc_x,a_idx,class_idxs):
     
     # concat ids and distances in a matrix
     m = np.concatenate((negs_dist,negs_idx))
+    print(m.shape)
 
     # sort descending by distance
     m = m[m[:,0].argsort()[::-1]]

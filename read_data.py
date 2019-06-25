c9b0a7628ec503cf6b1b083bff6d37f17852a3ce
Maria Cross <mscross92@googlemail.com>
2019-06-25 18:29:44 +0100

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -177,10 +177,10 @@ def get_hard_negs(desc_x,a_idx,class_idxs):
     print(m[1,1])
     m = m[:,0].argsort()
     print(m.shape)
-    print(m[0,0])
-    print(m[0,1])
-    print(m[1,0])
-    print(m[1,1])
+    print(m[0])
+    print(m[1])
+    # print(m[1,0])
+    # print(m[1,1])
 
     # return indices for first K neighbours - currently returning all!
     return m[1]
